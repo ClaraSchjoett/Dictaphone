a663fbafe87b6aeac58bb12698e72ec3c22dced4
Clara Schjoett <claraagerskov.schjoett@students.bfh.ch>
2019-06-07 15:58:34 +0200

    Testbench working!

--- fifo_tb.vhd
+++ fifo_tb.vhd
@@ -17,6 +17,7 @@
 -- Date        		Version		Author		Description
 -- 2019-06-01		1.0			Clara		Created
 -- 2019-06-02		1.1			Clara		Text I/O from files implemented
+-- 2019-06-07		1.1			Clara		Required functionalities implemented
 -------------------------------------------------------------------------------
 -- Inputs		:
 -- clk				Audio sampling rate
@@ -48,8 +49,8 @@ END fifo_tb;
 ARCHITECTURE behavior OF fifo_tb IS 
 
 	-- generics
-	constant ADDR_WIDTH		: positive 		:= 10;
-	constant DATA_WIDTH  	: positive 		:= 16;
+	constant ADDR_WIDTH		: positive := 10;
+	constant DATA_WIDTH  	: positive := 16;
 	
 	--Inputs
 	signal clk				: std_logic := '0';
@@ -69,12 +70,14 @@ ARCHITECTURE behavior OF fifo_tb IS
 	constant CLK_PERIOD 	: time := 20 ns;
 	
 	-- testbench signals
-	signal tests_done		: boolean := false;
-	signal write_done		: boolean := false;
-	signal read_done		: boolean := false;
-	signal full_boolean		: boolean := false;
-	signal empty_boolean	: boolean := false;
-
+	signal tests_done		: boolean := false; -- true when all test bench processes are finished
+	signal write_done		: boolean := false;	-- true when process writing to FIFO is finished
+	signal read_done		: boolean := false;	-- true when process reading from FIFO is finished
+	signal seq				: std_logic_vector(0 to 2);
+	signal seq_r			: std_logic_vector(0 to 2) := "000";
+	signal seq_w			: std_logic_vector(0 to 2) := "000";
+	
+	
 BEGIN
 	-- Instantiate the Device Under Test (DUT)
 	DUT: entity work.fifo
@@ -88,8 +91,7 @@ BEGIN
 			full		=> full,
 			empty		=> empty,
 			almost_empty=> almost_empty,
-			almost_full => almost_full
-		);
+			almost_full => almost_full);
 		
 	-- clock and reset generation
 	clk <= 		not clk after 0.5 * CLK_PERIOD when not tests_done else 
@@ -98,46 +100,83 @@ BEGIN
 				'0' after 0.25 * CLK_PERIOD,
 				'1' after 1.75 * CLK_PERIOD;
 	
-	-- Need to convert std_logic signals to boolean because while loop calls for boolean expression
-	full_boolean <= true when full = '1' else false;
-	empty_boolean <= true when empty = '1' else false;
+	-- Compares seq_w and seq_r, assigns larger value to seq (if statement must be within a process)
+	MAX_PROC : process(CLK)
+	begin
+		if unsigned(seq_w) > unsigned(seq_r) then
+			seq <= seq_w;
+		else
+			seq <= seq_r;
+		end if;
+	end process MAX_PROC;
 	
 	-- Stimuli generation: Write into FIFO
 	WRITE_PROC : process		
 		-- File and line buffer declaration
 		file dataIn			: text;
 		variable line_in	: line;
-		variable data 		: std_logic_vector(DATA_WIDTH-1 downto 0);	--(0 to DATA_WIDTH-1);	
+		variable data 		: std_logic_vector(DATA_WIDTH-1 downto 0);	
 	begin		
 		
-		wait for CLK_PERIOD * 3;
+		wait for CLK_PERIOD * 3;						-- wait for reset
 	
 		-- Open source text file
 		file_open(dataIn, "data_in.txt", read_mode);	-- dataIn is the file handle for the text file containing test data
-			
+		
+		-- SEQUENCE 0
 		-- This while loop writes data with system clock rate into FIFO
-		while full_boolean = false loop 				-- line number is looped automatically
-			if not endfile(dataIn) then
-				readline(dataIn, line_in); 					-- parameter: file handle, line number (iterated automatically)
-				read(line_in, data);					
-				wait until rising_edge(clk);
-					wr <= '1';
-					data_in <= data;					-- Write data to FIFO
-			end if;
-			--wait until rising_edge(CLK);
-			--	wr <= '0';
-			--	data_in <= data;
-			-- wait until rising_edge(CLK)
-				-- wr <= '0';
-			
-		end loop;
+		if seq = "000" then								-- Sequence is incremented when FIFO is full
+			L1 : loop
+				exit L1 when seq > "000";
+				if not endfile(dataIn) then
+					readline(dataIn, line_in); 			-- parameter: file handle, line number (iterated automatically)
+					read(line_in, data);					
+					wait until rising_edge(clk);
+						wr <= '1';
+						data_in <= data;				-- Write data to FIFO
+				end if;
+				if full = '1' then
+					seq_w <= "001";						-- Increment sequence to 1	
+				end if;
+			end loop;
+			wr <= '0';
+		end if;
 		
-		-- Close text file (good practice to close files after use!)
-		file_close(dataIn);
+		-- SEQUENCE 1
+		wait until empty = '1';
+		seq_w <= "010";									-- Increment sequence to 2
+		wait for CLK_PERIOD * 1;
+	
+		-- SEQUENCE 2, 3, 4 and 5
+		-- This while loop writes data with 1/3 system clock rate into FIFO
+		if seq > "001" and seq < "110" then				-- Sequence is incremented by reading process
+			L2 : loop									
+				exit L2 when seq > "101";
+				wait until rising_edge(CLK);
+				wr <= '0';
+				wait until rising_edge(CLK);
+				wait until rising_edge(CLK);
+					if not endfile(dataIn) then
+						readline(dataIn, line_in); 		-- parameter: file handle, line number (iterated automatically)
+						read(line_in, data);					
+							wr <= '1';
+							data_in <= data;			-- Write data to FIFO
+					end if;
+				if almost_full = '1' and seq = "010" then
+					seq_w <= "011";							-- Increment sequence to 3
+				end if;
+				if almost_full = '1' and seq = "100" then	
+					seq_w <= "101";							-- Increment sequence to 5
+				end if;	
+			end loop;
+			wr <= '0';
+		end if;
+					
+		file_close(dataIn);								-- Close text file (good practice to close files after use!)
 		
-		write_done <= true;
+		write_done <= true;								-- Set flag for CHK process
 		
-		wait;	-- forever, do not repeat process
+		--wait;	-- forever, do not repeat process
 	end process WRITE_PROC;
 	
 	-- Stimuli generation: Read out of FIFO
@@ -147,27 +186,77 @@ BEGIN
 		variable line_out	: line;
 	begin
 		
-		wait for CLK_PERIOD * 8;
+		wait for CLK_PERIOD * 8;						-- wait for reset and valid value in FIFO
+		
 		-- Open destination text file
 		file_open(dataOut,"data_out.txt", write_mode);	-- dataOut is the file handle for the text file where FIFO data is written to
+		
+		-- SEQUENCE 0 and 1
 		-- This while loop reads data from FIFO with 1/3 of system clock rate
-		while empty_boolean = false loop 				-- line number is looped automatically
-			wait until rising_edge(CLK);
-			
-			wait until rising_edge(CLK);
-			rd <= '1';									-- rd must be '1' on pos. clock edge to clock out data
-			wait until rising_edge(CLK);				-- Data is clocked out because rd is '1'
-			if data_out /= "UUUUUUUUUUUUUUUU" then
+		if seq = "000" or seq = "001" then
+			L1: loop 										-- line number is looped automatically
+				exit L1 when seq > "001";
+				wait until rising_edge(CLK);
+				
+				wait until rising_edge(CLK);
+				rd <= '1';									-- rd must be '1' on pos. clock edge to clock out data
+				wait until rising_edge(CLK);				-- Data is clocked out because rd is '1'
+				if data_out /= "UUUUUUUUUUUUUUUU" then
+					rd <= '1';
+					write(line_out, data_out); 				-- Write data in FIFO to line_out
+					writeline(dataOut, line_out);			-- Write line_out to text file "data_out.txt"
+				end if;
+				rd <= '0';
+			end loop;
+		end if;
+		
+		wait for CLK_PERIOD * 1;
+		
+		-- SEQUENCE 3		
+		-- This while loop reads data from FIFO with system clock rate
+		if seq = "011" then
+			L2: loop										
+				exit L2 when seq = "100"; 
 				rd <= '1';
-				write(line_out, data_out); 				-- Write data in FIFO to line_out
-				writeline(dataOut, line_out);			-- Write line_out to text file "data_out.txt"
-			end if;
-			rd <= '0';
-		end loop;
+				wait until rising_edge(CLK);				-- Data is clocked out because rd is '1'
+				if data_out /= "UUUUUUUUUUUUUUUU" then
+					rd <= '1';
+					write(line_out, data_out); 				-- Write data in FIFO to line_out
+					writeline(dataOut, line_out);			-- Write line_out to text file "data_out.txt"
+				end if;
+				rd <= '0';
+				if almost_empty = '1' and seq = "011" then
+					seq_r <= "100"; 							--4
+				end if;
+			end loop;
+		end if;
+		
+		-- SEQUENCE 5		
+		-- This while loop reads data from FIFO with system clock rate
+		if seq = "101" then
+			L3: loop	
+				exit L3 when seq > "101";
+				rd <= '1';
+				wait until rising_edge(CLK);				-- Data is clocked out because rd is '1'
+				if data_out /= "UUUUUUUUUUUUUUUU" then
+					rd <= '1';
+					write(line_out, data_out); 				-- Write data in FIFO to line_out
+					writeline(dataOut, line_out);			-- Write line_out to text file "data_out.txt"
+				end if;
+				rd <= '0';
+				if almost_empty = '1' and seq = "101" then
+					seq_r <= "110";								--6;
+				end if;
+			end loop;
+		end if;
+		
+		wait for CLK_PERIOD * 1;
 		
 		file_close(dataOut);
-		read_done <= true;
-		wait;	-- forever, do not repeat process
+		
+		read_done <= true;									-- Set flag for CHK process
+		
+		--wait;	-- forever, do not repeat process
 	end process READ_PROC;
 	
 	-- Monitoring and checks
@@ -201,8 +290,7 @@ BEGIN
 			file_open(dataOut,"data_out.txt", read_mode);	-- dataOut is the file handle for the text file where FIFO data was written to
 			
 			
-			while not endfile(dataOut) loop 					-- line number is looped automatically
-				--if not endfile(dataOut) then
+			while not endfile(dataOut) loop 				-- line number is looped automatically
 					readline(dataIn, line_in); 				-- parameter: file handle, line number (iterated automatically)
 					read(line_in, PLACEHOLDER1);				
 
@@ -212,7 +300,6 @@ BEGIN
 					compare(PLACEHOLDER1, PLACEHOLDER2);
 					
 					wait for CLK_PERIOD * 1;
-				--end if;
 				
 			end loop;
 			
