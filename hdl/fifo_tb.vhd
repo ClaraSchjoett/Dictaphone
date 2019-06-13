ba55ed37fc4ca9272fea17b7224f853f3ed4566b
Clara Schjoett <claraagerskov.schjoett@students.bfh.ch>
2019-06-13 11:22:24 +0200

    FIFO testbench: flag assertions implemente
    Timing errors!! We need to work these out!

--- hdl/fifo_tb.vhd
+++ hdl/fifo_tb.vhd
@@ -20,18 +20,7 @@
 -- 2019-06-07		1.1			Clara		Required functionalities implemented
 -------------------------------------------------------------------------------
 -- Inputs		:
--- clk				Audio sampling rate
--- reset			Resets the internal pointers and sets data to zero
--- data_in			Input Data
--- wr				write input: if wr is high, data_in is written on positive edge of clk
--- rs				read input: if rd is high, data appears on data_out on positive edge of clk
---
 -- Outputs		:
--- data_out			Output Data
--- full				is high if fifo-buffer is full
--- empty			is high if fifo-buffer is empty
--- almost_full		is high if fifo-buffer is more than 75% full
--- almost_empty		is high if fifo-buffer is less than 25% full
 -------------------------------------------------------------------------------
 
 LIBRARY IEEE;
@@ -48,50 +37,63 @@ END fifo_tb;
 
 ARCHITECTURE behavior OF fifo_tb IS 
 
-	-- generics
-	constant ADDR_WIDTH		: positive := 10;
-	constant DATA_WIDTH  	: positive := 16;
+	-- Generics
+	constant ADDR_WIDTH			: positive 	:= 10;
+	constant DATA_WIDTH  		: positive 	:= 16;
+	constant ALMOST_FULL_RATIO	: real 		:= 0.75;
+	constant ALMOST_EMPTY_RATIO	: real		:= 0.25;
 	
 	--Inputs
-	signal clk				: std_logic := '0';
-	signal reset			: std_logic := '1';
-	signal data_in			: std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
-	signal rd				: std_logic := '0';
-	signal wr				: std_logic := '0';
+	signal clk					: std_logic := '0';
+	signal reset				: std_logic := '1';
+	signal data_in				: std_logic_vector(DATA_WIDTH-1 downto 0);
+	signal rd					: std_logic := '0';
+	signal wr					: std_logic := '0';
 	
 	--Outputs
-	signal data_out			: std_logic_vector(DATA_WIDTH-1 downto 0);
-	signal empty			: std_logic;
-	signal full				: std_logic;
-	signal almost_empty		: std_logic;
-	signal almost_full		: std_logic;
+	signal data_out				: std_logic_vector(DATA_WIDTH-1 downto 0);
+	signal empty				: std_logic;
+	signal full					: std_logic;
+	signal almost_empty			: std_logic;
+	signal almost_full			: std_logic;
 	
 	-- Clock period and other time definitions
-	constant CLK_PERIOD 	: time := 20 ns;
+	constant CLK_PERIOD 		: time 		:= 20 ns;
+	
+	-- Constants calculated from generic values
+	constant COUNT_TOTAL		: integer 	:= integer(2**ADDR_WIDTH - 1);
+	constant COUNT_ALMOST_EMPTY	: integer 	:= integer(ALMOST_EMPTY_RATIO*real(2**ADDR_WIDTH)-real(1));
+	constant COUNT_ALMOST_FULL 	: integer 	:= integer(ALMOST_FULL_RATIO*real(2**ADDR_WIDTH)-real(1));
 	
 	-- testbench signals
-	signal tests_done		: boolean := false; -- true when all test bench processes are finished
-	signal write_done		: boolean := false;	-- true when process writing to FIFO is finished
-	signal read_done		: boolean := false;	-- true when process reading from FIFO is finished
-	signal seq				: std_logic_vector(0 to 2);
-	signal seq_r			: std_logic_vector(0 to 2) := "000";
-	signal seq_w			: std_logic_vector(0 to 2) := "000";
+	signal tests_done			: boolean 	:= false; 				-- true when all test bench processes are finished
+	signal write_done			: boolean 	:= false;				-- true when process writing to FIFO is finished
+	signal read_done			: boolean 	:= false;				-- true when process reading from FIFO is finished
+	signal seq					: std_logic_vector(0 to 2);			-- current testbench sequence
+	signal seq_r				: std_logic_vector(0 to 2) := "000";-- testbench sequence for reading process
+	signal seq_w				: std_logic_vector(0 to 2) := "000";-- testbench sequence for writing process
+	signal word_counter     	: integer := 0; -- range 0 to 1023 := 0;		-- number of words in FIFO
 	
 	
 BEGIN
 	-- Instantiate the Device Under Test (DUT)
 	DUT: entity work.fifo
-		PORT MAP (
-			clk			=> clk,
-			reset		=> reset,
-			data_in		=> data_in,
-			wr			=> wr,
-			rd			=> rd,
-			data_out	=> data_out,
-			full		=> full,
-			empty		=> empty,
-			almost_empty=> almost_empty,
-			almost_full => almost_full);
+		generic map(
+			ADDR_WIDTH			=> ADDR_WIDTH,
+			DATA_WIDTH			=> DATA_WIDTH,
+			ALMOST_FULL_RATIO	=> ALMOST_FULL_RATIO,
+			ALMOST_EMPTY_RATIO	=> ALMOST_EMPTY_RATIO)
+		port map(
+			clk					=> clk,
+			reset				=> reset,
+			data_in				=> data_in,
+			wr					=> wr,
+			rd					=> rd,
+			data_out			=> data_out,
+			full				=> full,
+			empty				=> empty,
+			almost_empty		=> almost_empty,
+			almost_full 		=> almost_full);
 		
 	-- clock and reset generation
 	clk <= 		not clk after 0.5 * CLK_PERIOD when not tests_done else 
@@ -99,16 +101,29 @@ BEGIN
 	reset <= 	'1', 
 				'0' after 0.25 * CLK_PERIOD,
 				'1' after 1.75 * CLK_PERIOD;
-	
+				
 	-- Compares seq_w and seq_r, assigns larger value to seq (if statement must be within a process)
-	MAX_PROC : process(CLK)
+	-- This process is necessary because we cant assign values to same signal (seq) from differenct processes (write and read process)
+	MAX_PROC : process(seq_w, seq_r)
 	begin
 		if unsigned(seq_w) > unsigned(seq_r) then
 			seq <= seq_w;
 		else
 			seq <= seq_r;
-		end if;
+		end if;	
 	end process MAX_PROC;
+	
+	-- Monitor value of word counter
+	COUNTER_PROC: process(wr, rd, clk)
+	begin
+		if clk'event and clk = '1' then
+			if wr = '1' and rd = '0' then
+				word_counter <= word_counter + 1;
+			elsif wr = '0' and rd = '1' then
+				word_counter <= word_counter - 1;
+			end if;										-- If both are high, dont change count of word_counter
+		end if;
+	end process COUNTER_PROC;
 	
 	-- Stimuli generation: Write into FIFO
 	WRITE_PROC : process		
@@ -128,34 +143,32 @@ BEGIN
 		if seq = "000" then								-- Sequence is incremented when FIFO is full
 			L1 : loop
 				exit L1 when seq > "000";
+				wait until falling_edge(clk);
 				if not endfile(dataIn) then
 					readline(dataIn, line_in); 			-- parameter: file handle, line number (iterated automatically)
 					read(line_in, data);					
-					wait until rising_edge(clk);
 						wr <= '1';
-						data_in <= data;				-- Write data to FIFO
+						data_in <= data;				-- Apply data to FIFO
 				end if;
 				if full = '1' then
+					wr <= '0';
 					seq_w <= "001";						-- Increment sequence to 1	
 				end if;
 			end loop;
 			wr <= '0';
 		end if;
 		
-		-- SEQUENCE 1
-		wait until empty = '1';
-		seq_w <= "010";									-- Increment sequence to 2
-		wait for CLK_PERIOD * 1;
-	
+		wait until seq = "010";
+		
 		-- SEQUENCE 2, 3, 4 and 5
 		-- This while loop writes data with 1/3 system clock rate into FIFO
 		if seq > "001" and seq < "110" then				-- Sequence is incremented by reading process
 			L2 : loop									
 				exit L2 when seq > "101";
-				wait until rising_edge(CLK);
+				wait until falling_edge(CLK);
 				wr <= '0';
-				wait until rising_edge(CLK);
-				wait until rising_edge(CLK);
+				wait until falling_edge(CLK);
+				wait until falling_edge(CLK);
 					if not endfile(dataIn) then
 						readline(dataIn, line_in); 		-- parameter: file handle, line number (iterated automatically)
 						read(line_in, data);					
@@ -176,7 +189,8 @@ BEGIN
 		
 		write_done <= true;								-- Set flag for CHK process
 		
-		--wait;	-- forever, do not repeat process
+		wait;	-- forever, do not repeat process
+		
 	end process WRITE_PROC;
 	
 	-- Stimuli generation: Read out of FIFO
@@ -196,21 +210,24 @@ BEGIN
 		if seq = "000" or seq = "001" then
 			L1: loop 										-- line number is looped automatically
 				exit L1 when seq > "001";
-				wait until rising_edge(CLK);
-				
-				wait until rising_edge(CLK);
-				rd <= '1';									-- rd must be '1' on pos. clock edge to clock out data
-				wait until rising_edge(CLK);				-- Data is clocked out because rd is '1'
-				if data_out /= "UUUUUUUUUUUUUUUU" then
-					rd <= '1';
+				wait until falling_edge(CLK);
+				wait until falling_edge(CLK);
+				if empty = '0' then
+					rd <= '1';								-- rd must be '1' on pos. clock edge to clock out data
+				end if;
+				wait until falling_edge(CLK);				-- Data is clocked out because rd is '1'
+				if data_out /= "UUUUUUUUUUUUUUUU" and empty = '0' then
 					write(line_out, data_out); 				-- Write data in FIFO to line_out
 					writeline(dataOut, line_out);			-- Write line_out to text file "data_out.txt"
 				end if;
 				rd <= '0';
+				if empty = '1' then
+					seq_r <= "010";
+				end if;
 			end loop;
 		end if;
 		
-		wait for CLK_PERIOD * 1;
+		wait until seq = "011";
 		
 		-- SEQUENCE 3		
 		-- This while loop reads data from FIFO with system clock rate
@@ -218,18 +235,20 @@ BEGIN
 			L2: loop										
 				exit L2 when seq = "100"; 
 				rd <= '1';
-				wait until rising_edge(CLK);				-- Data is clocked out because rd is '1'
+				wait until falling_edge(CLK);				-- Data is clocked out because rd is '1'
 				if data_out /= "UUUUUUUUUUUUUUUU" then
-					rd <= '1';
 					write(line_out, data_out); 				-- Write data in FIFO to line_out
 					writeline(dataOut, line_out);			-- Write line_out to text file "data_out.txt"
 				end if;
-				rd <= '0';
+				--rd <= '0';
 				if almost_empty = '1' and seq = "011" then
 					seq_r <= "100"; 							--4
 				end if;
 			end loop;
+			rd <= '0';
 		end if;
+		
+		wait until seq = "101";
 		
 		-- SEQUENCE 5		
 		-- This while loop reads data from FIFO with system clock rate
@@ -237,9 +256,8 @@ BEGIN
 			L3: loop	
 				exit L3 when seq > "101";
 				rd <= '1';
-				wait until rising_edge(CLK);				-- Data is clocked out because rd is '1'
+				wait until falling_edge(CLK);				-- Data is clocked out because rd is '1'
 				if data_out /= "UUUUUUUUUUUUUUUU" then
-					rd <= '1';
 					write(line_out, data_out); 				-- Write data in FIFO to line_out
 					writeline(dataOut, line_out);			-- Write line_out to text file "data_out.txt"
 				end if;
@@ -250,16 +268,45 @@ BEGIN
 			end loop;
 		end if;
 		
-		wait for CLK_PERIOD * 1;
-		
 		file_close(dataOut);
 		
 		read_done <= true;									-- Set flag for CHK process
 		
-		--wait;	-- forever, do not repeat process
+		wait;	-- forever, do not repeat process
+		
 	end process READ_PROC;
 	
-	-- Monitoring and checks
+	-- Checks if sginalisation outputs from FIFO are correct
+	MONITOR_SIGNALS: process(word_counter)
+	begin
+		
+		if data_out /= "UUUUUUUUUUUUUUUU" then
+
+			-- Check empty flag
+			assert (word_counter = 0 and empty = '1') or (word_counter /= 0 and empty = '0')
+			report "Flag empty is: " & std_logic'image(empty) & " but current FIFO content is: " & integer'image(word_counter)
+			severity error;
+			
+			-- Check almost_empty flag
+			assert (word_counter <= COUNT_ALMOST_EMPTY and almost_empty = '1') or (word_counter > COUNT_ALMOST_EMPTY and almost_empty = '0')
+			report "Flag almost_empty is: " & std_logic'image(almost_empty) & " but current FIFO content is: " & integer'image(word_counter)
+			severity warning;
+			
+			-- Check almost_full flag
+			assert (word_counter >= COUNT_ALMOST_FULL and almost_full = '1') or (word_counter < COUNT_ALMOST_FULL and almost_full = '0')
+			report "Flag almost_full is: " & std_logic'image(almost_full) & " but current FIFO content is: " & integer'image(word_counter)
+			severity warning;
+			
+			-- Check full flag
+			assert (word_counter = COUNT_TOTAL and full = '1') or (word_counter /= COUNT_TOTAL and full = '0')
+			report "Flag full is: " & std_logic'image(full) & " but current FIFO content is: " & integer'image(word_counter)
+			severity error;
+			
+		end if;
+		
+	end process MONITOR_SIGNALS;
+	
+	-- Checks written data against read data
 	CHK: process
 	
 		-- File and line buffer declaration
